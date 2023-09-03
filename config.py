2604edf27559c2743c95ac3f703903aa5e7b486d
YauHuiYi <87627036+YauHuiYi@users.noreply.github.com>
2023-09-03 23:34:37 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.czijrdsgxcij.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "yauhuiyi-employee"
+customregion = "us-east-1"
+
